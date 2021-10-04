2f37886151b178f0ee1442f4e78bc357ad2d3632
Grzegorz Sowinski <al-sowa@hotmail.com>
2021-10-04 17:14:00 +0200

    ekstra button

--- js/main.js
+++ js/main.js
@@ -3,66 +3,61 @@ $(document).ready(function() {
     let body = $('body');
     let btn = $('.btn');
 
-    let count = 0;
-
-
     $(btn).click(() => {
 
-        count += 1;
+        let container = $('div.container');
 
-        let container = $('<div class="container"></div>');
+        // console.log(container.length);
 
-        // if (count >= 2 && container.hasClass("container")) {
-        // container.addClass("open").removeClass("container");
-        // }
+        if (container.length) {
 
-        // if (count % 2 == 1) {
-        //     body.append(container);
-        // } else {
+            container.remove();
+            console.log('usuwam');
+            btn.text('Pobierz dane');
+        } else {
+            container = $('<div class="container"></div>');
+            btn.text('Next');
 
-        //     $(body).remove(container);
+            let paragraphUserId = $('<p id="Userid"></p>');
+            let paragraphId = $('<p id="id"></p>');
+            let paragraphTitle = $('<p id="title"></p>');
+            let paragraphBody = $('<p id="body"></p>');
 
-        // }
+            paragraphUserId.appendTo(container);
+            paragraphId.appendTo(container);
+            paragraphTitle.appendTo(container);
+            paragraphBody.appendTo(container);
+            container.appendTo(body);
 
-        let paragraphUserId = $('<p id="Userid"></p>');
-        let paragraphId = $('<p id="id"></p>');
-        let paragraphTitle = $('<p id="title"></p>');
-        let paragraphBody = $('<p id="body"></p>');
+            // $.get('https://akademia108.pl/api/ajax/get-post.php')
+            //     .done(function(data) {
 
-        paragraphUserId.appendTo(container);
-        paragraphId.appendTo(container);
-        paragraphTitle.appendTo(container);
-        paragraphBody.appendTo(container);
-        container.appendTo(body);
+            //         paragraphUserId.text('user Id: ' + data.id);
+            //         paragraphId.text('Id: ' + data.userId);
+            //         paragraphTitle.text('title: ' + data.title);
+            //         paragraphBody.text('body: ' + data.body);
 
-        // $.get('https://akademia108.pl/api/ajax/get-post.php')
-        //     .done(function(data) {
+            //     })
+            //     .fail(function(error) {
+            //        alert("Wystąpił błąd w połączeniu");
+            //     });
 
-        //         paragraphUserId.text('user Id: ' + data.id);
-        //         paragraphId.text('Id: ' + data.userId);
-        //         paragraphTitle.text('title: ' + data.title);
-        //         paragraphBody.text('body: ' + data.body);
-
-        //     })
-        //     .fail(function(error) {
-        //        alert("Wystąpił błąd w połączeniu");
-        //     });
-
-        $.getJSON('https://akademia108.pl/api/ajax/get-post.php')
-            .done((data) => {
+            $.getJSON('https://akademia108.pl/api/ajax/get-post.php')
+                .done((data) => {
 
 
-                paragraphUserId.text('user Id: ' + data.id);
-                paragraphId.text('Id: ' + data.userId);
-                paragraphTitle.text('title: ' + data.title);
-                paragraphBody.text('body: ' + data.body);
-                console.log(data);
+                    paragraphUserId.text('user Id: ' + data.id);
+                    paragraphId.text('Id: ' + data.userId);
+                    paragraphTitle.text('title: ' + data.title);
+                    paragraphBody.text('body: ' + data.body);
+                    console.log(data);
 
 
-            })
-            .fail(() => {
-                alert("Wystąpił błąd w połączeniu");
-            })
+                })
+                .fail(() => {
+                    alert("Wystąpił błąd w połączeniu");
+                })
+        }
 
     });
 
